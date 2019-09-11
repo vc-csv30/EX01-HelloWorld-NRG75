6de40fd20ad5168f6f0c635ee7c643c4d74dce7c
Nightmarejam <42326244+Nightmarejam@users.noreply.github.com>
2019-09-11 10:31:20 -0700

    I am not afraid of commitment.

--- src/main.cc
+++ src/main.cc
@@ -9,7 +9,7 @@
  * Statement of code ownership: I hereby state that I have written all of this
  * code and I have not copied this code from any other person or source.
  *
- * @author [CHANGE THIS INFORMATION]
+ * @author [Jesus Ayala]
  */
 
 #include <iostream>
@@ -19,3 +19,4 @@ int main(int argc, char *argv[]) {
     std::cout << "Hello, World!" << std::endl;
     return EXIT_SUCCESS;
 }
+
